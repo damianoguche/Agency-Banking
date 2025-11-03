64204628603a9363f8b3a3a60796d98232128359
Damian Oguche <eguchealaldi@yahoo.com>
2025-11-03 15:54:16 +0100

    Removed files and folders not needed for production

--- SQL/ledger_check.sql
+++
@@ -1,8 +0,0 @@
-SELECT 
-  wallet_number,
-  SUM(CASE WHEN entry_type = 'credit' THEN amount ELSE -amount END) AS computed_balance
-FROM ledger_entries
-GROUP BY wallet_number
-HAVING computed_balance <> (
-  SELECT balance FROM wallets WHERE wallets.walletNumber = ledger_entries.wallet_number
-);
--- src/jobs/cron/scheduler.ts
+++
@@ -1,6 +0,0 @@
-import cron from "node-cron";
-import { reconcileLedgers } from "../remediate.ts";
-
-cron.schedule("0 2 * * *", async () => {
-  await reconcileLedgers();
-});
