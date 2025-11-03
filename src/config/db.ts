dbf745f7ce2f3f5c8b224d6d2509383fc3cb1ab2
Damian Oguche <eguchealaldi@yahoo.com>
2025-11-03 15:22:27 +0100

    Updated package and database settings

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,20 +1,48 @@
+// import { Sequelize } from "sequelize";
+// import dotenv from "dotenv";
+// dotenv.config();
+
+// const isProduction = process.env.NODE_ENV === "production";
+
+// // Setup DB (MySQL)
+// const sequelize = new Sequelize(
+//   process.env.DB_NAME as string,
+//   process.env.DB_USER as string,
+//   process.env.DB_PASS as string,
+//   {
+//     host: process.env.DB_HOST || "localhost",
+//     dialect: "mysql" as const,
+//     port: Number(process.env.DB_PORT || 3306),
+//     logging: false,
+
+//     dialectOptions: isProduction
+//       ? {
+//           ssl: {
+//             require: true,
+//             rejectUnauthorized: false
+//           }
+//         }
+//       : {}
+//   }
+// );
+
+// export default sequelize;
+
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
 dotenv.config();
 
 const isProduction = process.env.NODE_ENV === "production";
 
-// Setup DB (MySQL)
 const sequelize = new Sequelize(
   process.env.DB_NAME as string,
   process.env.DB_USER as string,
   process.env.DB_PASS as string,
   {
     host: process.env.DB_HOST || "localhost",
-    dialect: "mysql" as const,
-    port: Number(process.env.DB_PORT || 3306),
+    port: Number(process.env.DB_PORT || 5432),
+    dialect: "postgres",
     logging: false,
-
     dialectOptions: isProduction
       ? {
           ssl: {
@@ -22,7 +50,7 @@ const sequelize = new Sequelize(
             rejectUnauthorized: false
           }
         }
-      : {}
+      : {} // no SSL for local dev
   }
 );
 
