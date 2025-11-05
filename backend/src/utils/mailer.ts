47597e7b50382f439fe2e4aaae4bdfa261e834c0
Damian Oguche <eguchealaldi@yahoo.com>
2025-11-05 21:16:23 +0100

    Updated mailer.ts

--- backend/src/utils/mailer.ts
+++ backend/src/utils/mailer.ts
@@ -43,7 +43,7 @@ export async function sendWalletEmail(
   walletNumber: string
 ) {
   await resend.emails.send({
-    from: `"SecureBank <${process.env.SMTP_USER}>"`,
+    from: "SecureBank <onboarding@resend.dev>",
     to,
     subject: "Welcome to SecureBank — Your Wallet Has Been Created",
     html: `
